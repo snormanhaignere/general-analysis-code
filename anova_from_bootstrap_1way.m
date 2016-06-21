9bab42f64c5f831930d944cb940bc84195e04761
svnh <svnh@mit.edu>
2016-06-20 20:52:53 -0400

    created anova_from_bootstrap_1way

--- anova_from_bootstrap_1way.m
+++ anova_from_bootstrap_1way.m
@@ -1,6 +1,6 @@
-function p = anova_from_bootstrap_1way(X)
+function p = anova_from_bootstrap_1way(X, empirical_variance)
 
-% Performs an analysis similar to an 1-way ANOVA but suited for bootstrapped
+% Performs an analysis similar to a 1-way ANOVA but suited for bootstrapped
 % samples. Assumes the input matrix X contains sample means, calculated using
 % bootstrapping. Columns are assumed to correspond to different conditions and
 % rows to samples.
@@ -8,16 +8,12 @@ function p = anova_from_bootstrap_1way(X)
 assert(all(~isnan(X(:))));
 dims = size(X);
 
-% demean entire matrix
-X_matrix_demeaned = X - mean(X(:));
-
 % demean columns
 column_means = mean(X,1);
 X_col_demeaned = X - repmat(column_means, [dims(1), ones(size(dims(2:end)))]);
 
-% null and sample statistic
-null = sum(X_col_demeaned.^2, 2);
-sample_stat = mean( sum(X_matrix_demeaned.^2, 2), 1);
+% null variance
+null_variance = var(X_col_demeaned, [], 2);
 
 % p-value
-p = mean(sample_stat < null);
+p = mean(empirical_variance < null_variance);
--- scratch.m
+++ scratch.m
@@ -1,8 +1,7 @@
-sig = rand(100,5);
-X = randn(100,5) + sig;
-Y = randn(100,5) + sig;
-diag(corr(X,Y))'
-sqerr_norm(zscore(X), zscore(Y))
-sqerr_norm(X,Y)
-sqerr_norm(X*10,Y)
-sqerr_norm(X+10,Y+10)
+
+
+N = 1e3;
+sample_size = 10;
+P = 5;
+X = randn(N, P) + ones(N,1)*randn(1,P)*2;
+anova_from_bootstrap_1way(X, median(var(X,[],2)))
