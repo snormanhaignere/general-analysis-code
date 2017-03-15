a72d62bef57e5ef029dad62b86e32fe81b1f65c2
svnh <svnh@mit.edu>
2017-03-15 16:52:14 -0400

    Modified: regress_predictions_parallelize_with_slurm.m
    Can now overwrite files, and can skip using batch.

--- regress_predictions_parallelize_with_slurm.m
+++ regress_predictions_parallelize_with_slurm.m
@@ -1,6 +1,8 @@
 function [Yh, test_folds] = regress_predictions_parallelize_with_slurm(...
     F, Y, test_folds, method, K, train_folds, output_directory, varargin)
 
+% 2017-03-15: Added capacity to overwrite files, Sam NH
+
 addpath('/mindhive/nklab/u/svnh/sbatch-code-v2');
 
 % batch parameters
@@ -11,35 +13,59 @@ B.std_feats = true;
 B.groups = [];
 B.batch_size = 1000;
 B.max_run_time_in_min = num2str(60*10);
+B.overwrite = false;
+B.use_sbatch = true;
 B = parse_optInputs_keyvalue(varargin, B);
 
 n_batches = ceil(size(Y,2) / B.batch_size);
 
 Yh = nan(size(Y));
+overwritten = false(1, n_batches);
 while true
         
-    finished_batch = false(1,n_batches);
+    finished_batch = false(1, n_batches);
     for i = 1:n_batches
+        
         yi = (1:B.batch_size) + (i-1) * B.batch_size;
         yi(yi > size(Y,2)) = [];
         MAT_file = [output_directory '/predictions' ...
             num2str(yi(1)) '-' num2str(yi(end)) '.mat'];
         
+        % overwrite once
+        if B.overwrite && ~overwritten(i)
+            if exist(MAT_file, 'file')
+                delete(MAT_file)
+            end
+            overwritten(i) = true;
+        end
+        
         if ~exist(MAT_file, 'file')            
             
-            % set the job id
-            B.job_id = ['sb' num2str(i)];
+            % indicate progress
+            fprintf('Batch %d of %d: %d - %d\n', i, n_batches, yi(1), yi(end));
+            drawnow;
             
-            % matlab function, arguments and directory to call the function from
-            B.matlab_fn = @regress_predictions_from_3way_crossval;
-            B.matlab_fn_args = {F, Y(:,yi), test_folds, method, K,...
-                train_folds, MAT_file, B.std_feats, B.groups};
-            B.directory_to_run_from = '/mindhive/nklab/u/svnh/general-analysis-code';
-            
-            % call the sbatch
-            call_sbatch_smart(B);
+            if B.use_sbatch
+                % set the job id
+                B.job_id = ['sb' num2str(i)];
+                
+                % matlab function, arguments and directory to call the function from
+                B.matlab_fn = @regress_predictions_from_3way_crossval;
+                B.matlab_fn_args = {F, Y(:,yi), test_folds, method, K,...
+                    train_folds, MAT_file, B.std_feats, B.groups};
+                B.directory_to_run_from = '/mindhive/nklab/u/svnh/general-analysis-code';
+                
+                % call the sbatch
+                call_sbatch_smart(B);
+                
+            else
+                regress_predictions_from_3way_crossval(F, Y(:,yi), ...
+                    test_folds, method, K, train_folds, MAT_file, ...
+                    B.std_feats, B.groups);
+            end
             
         else
+            
             % try to read the file for 5 minutes ..
             % (file might exist but not be fully written to)
             tic;
@@ -58,6 +84,7 @@ while true
             if ~finished_batch(i)
                 delete(MAT_file);
             end
+            
         end
         clear yi;
         
