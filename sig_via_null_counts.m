2015b2ad74c5fe40d95dc067b717cac7d9ed5cec
Sam Norman-Haignere <snormanhaignere@gmail.com>
2017-03-18 00:14:23 -0400

    Small changes: sig_via_null_counts.m, sig_via_null_gaussfit.m

--- sig_via_null_counts.m
+++ sig_via_null_counts.m
@@ -1,9 +1,22 @@
-function signed_log10p = sig_via_null_counts(stat, null_stat, varargin)
+function signed_log10p = sig_via_null_counts(test_stat, null_samples, varargin)
 
 % Computes a measure of significance for a sample statistic using samples from
 % the null (e.g. computed via a permutation test). P-values are computed by
 % counting the fraction of samples from the null that exceed the test statistic
 % in magnitude. Signed -log10[p] values are returned
+% 
+% -- Inputs -- 
+% 
+% test_stat: N1 x N2 ... array
+% 
+% null_samples: samples x N1 x N2 ... array
+% 
+% -- Optional Arguments -- 
+% 
+% Optional arguments are specified as name-value pairs, e.g:
+% normalized_correlation_v2(X, Y, 'NAME', 'VALUE', ...) 
+% 
+% tail: 'left', 'right', or 'both' (default)
 % 
 % % -- Guassian example -- 
 % N = 100e3;
@@ -11,41 +24,63 @@ function signed_log10p = sig_via_null_counts(stat, null_stat, varargin)
 % null_smps = normrnd(0,1,[N,1]) * sqrt(variances);
 % 
 % estimated_signed_log10p = sig_via_null_counts([3 3 3], null_smps)
-% analytic_log10p = -log10(2*normcdf(-[3 3 3], [0 0 0], sqrt(variances)))'
+% analytic_log10p = -log10(2*normcdf(-[3 3 3], [0 0 0], sqrt(variances)))
+% estimated_signed_log10p = sig_via_null_counts([3 3 3], null_smps, 'tail', 'right')
+% analytic_log10p = -log10(normcdf(-[3 3 3], [0 0 0], sqrt(variances)))
 % 
 % 2017-03-17: Added some additional error checks
 
-if isvector(stat) && ismatrix(null_stat)
-    stat = stat(:);
+I.tail = 'both';
+I = parse_optInputs_keyvalue(varargin, I);
+
+% dimensions of the input statistics
+stat_dims = size(test_stat);
+
+% convert vector to column vector
+if isvector(test_stat) && ismatrix(null_samples)
+    test_stat = test_stat(:);
 end
 
 % check correspondence of dimensions between stat and null_stat
-for i = 2:ndims(null_stat)
-    if ~(size(null_stat,i) == size(stat,i-1));
+for i = 2:ndims(null_samples)
+    if ~(size(null_samples,i) == size(test_stat,i-1));
         error('Dimensions of test statistic don''t match with those of the null');
     end
 end
 
 % dimensions
-dims_stat = size(stat);
-n_perms = size(null_stat,1);
-if n_perms < 10
+n_null_smps = size(null_samples,1);
+if n_null_smps < 10
     error('Should be more than 10 samples.');
 end
 
-% replicate stat over the permuted dimension
-stat_rep = repmat( shiftdim(stat, -1),  [n_perms, ones(1,ndims(stat))]);
+% replicate stat over the dimension
+stat_rep = repmat( shiftdim(test_stat, -1),  [n_null_smps, ones(1,ndims(test_stat))]);
 
 % calculate two-tailed p-value
-p = min(mean(null_stat > stat_rep), mean(null_stat < stat_rep)) * 2;
+switch I.tail
+    case 'both' % 'two-tailed'
+        p = min(mean(null_samples > stat_rep, 1), mean(null_samples < stat_rep, 1)) * 2;
+        
+    case 'right'
+        p = mean(null_samples > stat_rep, 1);
+        
+    case 'left'
+        p = mean(null_samples < stat_rep, 1);
+        
+    otherwise
+        error('No matching case for I.tail value %s', I.tail);
+end
 
-% remove singleton dimension
-p = reshape(p, dims_stat);
+% remove first dimension
+p = squeeze_dims(p, 1);
 
 % set p-values of zero to the smallest possible p-value
-p(p==0) = 1/n_perms;
+p(p==0) = 1/n_null_smps;
 
 % convert to signed -log10[p]
-above_or_below_null = sign(stat - squeeze_dims(median(null_stat),1));
+above_or_below_null = sign(test_stat - squeeze_dims(median(null_samples),1));
 signed_log10p = above_or_below_null .* -log10(p);
 
+% reshape back to dimensions of stat
+signed_log10p = reshape(signed_log10p, stat_dims);
--- sig_via_null_gaussfit.m
+++ sig_via_null_gaussfit.m
@@ -1,10 +1,24 @@
-function [signed_log10p, z] = sig_via_null_gaussfit(stat, null_stat, varargin)
+function [signed_log10p, z] = sig_via_null_gaussfit(...
+    test_stat, null_samples, varargin)
 
 % Computes a measure of significance for a sample statistic using samples from
 % the null (e.g. computed via a permutation test). P-values are computed by
 % fitting a gaussian distribution to the null and estimating the tail
 % probability given this fit. This function assumes the samples are in the first
 % dimension of matrix null_stat (see example below).
+% 
+% -- Inputs -- 
+% 
+% test_stat: N1 x N2 ... array
+% 
+% null_samples: samples x N1 x N2 ... array
+% 
+% -- Optional Arguments -- 
+% 
+% Optional arguments are specified as name-value pairs, e.g:
+% normalized_correlation_v2(X, Y, 'NAME', 'VALUE', ...) 
+% 
+% tail: 'left', 'right', or 'both' (default)
 %
 % % -- Guassian example --
 % N = 100e3;
@@ -12,7 +26,9 @@ function [signed_log10p, z] = sig_via_null_gaussfit(stat, null_stat, varargin)
 % null_smps = normrnd(0,1,[N,1]) * sqrt(variances);
 %
 % estimated_signed_log10p = sig_via_null_gaussfit([3 3 3], null_smps)
-% analytic_log10p = -log10(2*normcdf(-[3 3 3], [0 0 0], sqrt(variances)))'
+% analytic_log10p = -log10(2*normcdf(-[3 3 3], [0 0 0], sqrt(variances)))
+% estimated_signed_log10p = sig_via_null_gaussfit([3 3 3], null_smps, 'tail', 'right')
+% analytic_log10p = -log10(normcdf(-[3 3 3], [0 0 0], sqrt(variances)))
 %
 % 2016-08-26: Changed code so that the returned vector/matrix is of the same
 % dimesions as the input statistic. Also added an error check.
@@ -27,37 +43,39 @@ function [signed_log10p, z] = sig_via_null_gaussfit(stat, null_stat, varargin)
 % 
 % 2017-02-24: Bug fix: z-stat was originally not being reshaped before being
 % returned
+% 
+% 2017-03-17: Very minor tweaks
 
 I.tail = 'both';
 I = parse_optInputs_keyvalue(varargin, I);
 
 % dimensions of the input statistics
-stat_dims = size(stat);
+stat_dims = size(test_stat);
 
 % convert all vectors to column vectors
-if isvector(stat) && ismatrix(null_stat)
-    stat = stat(:);
+if isvector(test_stat) && ismatrix(null_samples)
+    test_stat = test_stat(:);
 end
 
 % check correspondence of dimensions between stat and null_stat
-for i = 2:ndims(null_stat)
-    if ~(size(null_stat,i) == size(stat,i-1));
+for i = 2:ndims(null_samples)
+    if ~(size(null_samples,i) == size(test_stat,i-1));
         error('Dimensions of test statistic don''t match with those of the null');
     end
 end
 
 % number of samples
-n_samples = size(null_stat,1);
-if n_samples < 10
+n_null_samples = size(null_samples,1);
+if n_null_samples < 10
     error('Should be more than 10 samples.');
 end
 
 % mean and standard deviation of the null
-m = squeeze_dims(mean(null_stat,1),1);
-sd = squeeze_dims(std(null_stat,[],1),1);
+m = squeeze_dims(mean(null_samples,1),1);
+sd = squeeze_dims(std(null_samples,[],1),1);
 
 % z-value of test statistic
-z = (stat - m)./sd;
+z = (test_stat - m)./sd;
 
 switch I.tail
     case 'both' % 'two-tailed'
