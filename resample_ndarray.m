dd10d47020eac34121d1cb7f5f1198616f5dc6ac
Sam Norman-Haignere <snormanhaignere@gmail.com>
2017-09-27 11:19:03 +0200

    Minor change to resample_ndarray.m

--- resample_ndarray.m
+++ resample_ndarray.m
@@ -1,6 +1,24 @@
-function Y = resample_ndarray(X, new_sr, old_sr)
+function X = resample_ndarray(X, new_sr, old_sr, DIM)
 % Wrapper function for the resample function that works for nd-arrays operating over the first
 % dimension.
+% 
+% 2017-06-30: Modified to accept the dimension over which to resample
+
+if nargin < 4
+    DIM = 1;
+end
+
+% permute chosen dimsnion to front;
+perm_order = [DIM, setdiff(1:ndims(X), DIM)];
+X = permute(X, perm_order);
+
+% resample
+X = resamp_first_dim(X, new_sr, old_sr);
+
+% permute back
+X = ipermute(X, perm_order);
+
+function Y = resamp_first_dim(X, new_sr, old_sr)
 
 dims = size(X);
 X = reshape(X, [dims(1),prod(dims(2:end))]);
