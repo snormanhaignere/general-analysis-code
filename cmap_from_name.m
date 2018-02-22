39c60a702bfe4f5e74853cd07b613b4846d76819
Sam Norman-Haignere <snormanhaignere@gmail.com>
2018-02-22 11:37:55 +0100

    cmap_from_name.m, added colormaps and documentation

--- cmap_from_name.m
+++ cmap_from_name.m
@@ -3,6 +3,23 @@ function cmap = cmap_from_name(cname)
 % Reads in a N x 3 colormap matrix from a color map name
 % 
 % 2017-03-17: Created, Sam NH
+% 
+% 2017-10-05: Generalized the color inversion functionality
+% 
+% -- Example --
+% 
+% figure;
+% imagesc(linspace(0,1,100));
+% colormap(cmap_from_name('lightblue-to-yellow1'));
+% figure;
+% imagesc(linspace(0,1,100));
+% colormap(cmap_from_name('lightblue-to-yellow1-inverted'));
+
+if strfind(cname, '-inverted')
+    cname = strrep(cname, '-inverted', '');
+    cmap = flipud(cmap_from_name(cname));
+    return;
+end
 
 % make sure auxilliary color maps are in path
 addpath([fileparts(which(mfilename)) '/python-maps']);
@@ -13,12 +30,9 @@ addpath([fileparts(which(mfilename)) '/pmkmp']);
 % color map and range of values to plot
 switch cname
     case 'lightblue-to-yellow1';
-        load(['colormap-custom-' cname '.mat']);
+        load(['colormap-custom-' cname '.mat'], 'cmap');
     case {'parula', 'jet'}
         cmap = colormap(cname);
-    case {'parula-inverted', 'jet-inverted'}
-        cmap = colormap(strrep(cname, '-inverted', ''));
-        cmap = flipud(cmap);
     case {'plasma'}
         cmap = plasma;
     case {'magma'}
@@ -27,6 +41,8 @@ switch cname
         cmap = inferno;
     case {'viridis'}
         cmap = viridis;
+    case {'cbrewer-Reds'}
+        cmap = cbrewer('seq', 'Reds', 128);
     otherwise
         error('No matching colormap');
 end
