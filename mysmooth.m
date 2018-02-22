6fd14b71b7d3ca20ad4157b53285e0102af9c497
Sam Norman-Haignere <snormanhaignere@gmail.com>
2018-02-22 11:34:20 +0100

    mysmooth.m
    Smoothing with gaussian kernel

---
+++ mysmooth.m
@@ -0,0 +1,23 @@
+function Ysmooth = mysmooth(Y, sr, fwhm_ms)
+
+% Smoothes every column of a data matrix by a Gaussian kernel
+% 
+% 2017-11-24: Created by Sam NHs
+
+% kernel
+fwhm_smp = fwhm_ms*sr/1000;
+sig = fwhm_smp / sqrt(8*log(2));
+x = (-round(sig*3):round(sig*3))';
+h = normpdf(x, 0, sig);
+h = h/sum(h);
+
+dims = size(Y);
+Y = reshape(Y, dims(1), prod(dims(2:end)));
+Ysmooth = nan(size(Y));
+for i = 1:prod(dims(2:end))
+    if ~any(isnan(Y(:,i)))
+        Ysmooth(:,i) = myconv(Y(:,i), h, 'causal', false);
+    end
+end
+
+Ysmooth = reshape(Ysmooth, dims);
