e63c15ddfb5e0d09b90680e191f916e0f6dbd124
Sam Norman-Haignere <snormanhaignere@gmail.com>
2018-05-28 12:36:49 +0200

    partition_within_labels : added functionality

--- partition_within_labels.m
+++ partition_within_labels.m
@@ -14,8 +14,13 @@ function partition_index = partition_within_labels(labels, partition_fractions, 
 % reshape(partition_index, 5, 3)
 % 
 % 2018-02-22: Created, Sam NH
+% 
+% 2018-04-11: Updated to work with cell arrays of labels, and to make shuffling
+% within a label group optional, also made it possible to specify an integer
+% argument in which case the partition is even amongst that many groups
 
 I.seed = [];
+I.shuffle = true;
 I = parse_optInputs_keyvalue(varargin, I);
 
 % check vector
@@ -26,18 +31,35 @@ if ~isempty(I.seed)
     ResetRandStream2(I.seed);
 end
 
+% turn integer into probability vector
+if isscalar(partition_fractions)
+    partition_fractions = ones(1, partition_fractions) / partition_fractions;
+end
+
+% unique labels, sort by frequency
+unique_labels = unique(labels);
+n_unique_labels = length(unique_labels);
+n_instances_per_label = nan(1, n_unique_labels);
+for i = 1:n_unique_labels
+    n_instances_per_label(i) = sum(ismember(labels, unique_labels(i)));
+end
+[~, xi] = sort(n_instances_per_label, 'ascend');
+unique_labels = unique_labels(xi);
+clear xi;
+
 n_labels = length(labels);
-n_unique_labels = length(unique(labels));
 n_partitions = length(partition_fractions);
 partition_index = nan(n_labels,1);
 for i = 1:n_unique_labels
 
     % indices for this label
-    xi = find(labels == i);
+    xi = find(ismember(labels, unique_labels(i)));
 
     % shuffle the indices
-    xi = Shuffle(xi);
-
+    if I.shuffle
+        xi = xi(randperm(length(xi)));
+    end
+    
     count = 0;
     for j = 1:n_partitions
 
